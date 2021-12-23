5f64cf6f9a39eff90e2d968060a2df80991ed818
MohamedSabil83 <me@mohamedsabil83.com>
2021-12-23 04:30:38 +0300

    Update tests to match pestphp

--- tests/DateTest.php
+++ tests/DateTest.php
@@ -1,64 +1,25 @@
 <?php
 
-namespace Mohamedsabil83\LaravelHijrian\Tests;
-
-use Hijrian;
 use Carbon\Carbon;
+use Mohamedsabil83\LaravelHijrian\Facades\LaravelHijrian;
 
-class DateTest extends TestCase
-{
-    private $hijri;
-    private $gregory;
+beforeEach(function () {
+    $this->gregory = '2019-08-09';
+    $this->hijri = '1440-12-08';
+});
 
-    public function __construct()
-    {
-        parent::__construct();
+it('Convert Gregory To Hijri', function () {
+    expect(LaravelHijrian::hijri($this->gregory)->format('Y-m-d'))->toEqual($this->hijri);
+});
 
-        $this->hijri = '1440-12-08';
-        $this->gregory = '2019-08-09';
-    }
+it('Convert Hijri To Gregory', function () {
+    expect(LaravelHijrian::gregory($this->hijri)->format('Y-m-d'))->toEqual($this->gregory);
+});
 
-    /**
-     * @test Convert Gregory To Hijri.
-     *
-     * @return void
-     */
-    public function convert_gregory_to_hijri()
-    {
-        $hijri = Hijrian::hijri($this->gregory);
-        $this->assertSame($this->hijri, $hijri->format('Y-m-d'));
-    }
+it('Return A Grogory Date', function () {
+    expect(LaravelHijrian::gregory())->toBeInstanceOf(Carbon::class);
+});
 
-    /**
-     * @test Convert Hijri To Gregory.
-     *
-     * @return void
-     */
-    public function convert_hijri_to_gregory()
-    {
-        $gregory = Hijrian::gregory($this->hijri);
-        $this->assertSame($this->gregory, $gregory->format('Y-m-d'));
-    }
-
-    /**
-     * @test Get Gregory date.
-     *
-     * @return void
-     */
-    public function get_grogory_date()
-    {
-        $gregory = Hijrian::gregory();
-        $this->assertInstanceOf(Carbon::class, $gregory);
-    }
-
-    /**
-     * @test Get Hijri date.
-     *
-     * @return void
-     */
-    public function get_hijri_date()
-    {
-        $hijri = Hijrian::hijri();
-        $this->assertInstanceOf(Carbon::class, $hijri);
-    }
-}
+it('Return A Hijri Date', function () {
+    expect(LaravelHijrian::hijri())->toBeInstanceOf(Carbon::class);
+});
